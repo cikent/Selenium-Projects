f3065490a04cf7cf64e81a28b34e410b10b6543a
cikent <christopherikent@gmail.com>
2020-03-25 07:34:54 -0700

    Finished Chapter 4, resolved the import error and added Comments
    Signed-off-by: cikent <christopherikent@gmail.com>

--- Selenium-PythonProjects/python-webui-testing/tests/test_web.py
+++ Selenium-PythonProjects/python-webui-testing/tests/test_web.py
@@ -1,18 +1,24 @@
+"""
+This module contains web test cases for the tutorial.
+Tests use Selenium WebDriver with Chrome and ChromeDriver.
+The fixtures set up and clean up the ChromeDriver instance.
+"""
+
 # Import the Pytest Library
 import pytest
 
 # From the Selenium Webdriver Library, import the Chrome module
 from selenium.webdriver import Chrome
 # From the Selenium Webdriver Common Keys Library, import the Keys module
-from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.keys import Keys
 
 # Create a Pytest Fixture to handle Setup and Cleanup of Tests
 @pytest.fixture                     # Pytest Decorator
 def browser():                      # Define the function
-    driver = Chrome()               # Create a Chrome browser Object
+    driver = Chrome()               # Initialize ChromeDriver by creating a Chrome browser object
     driver.implicitly_wait(10)      # Set the Driver's Implicit Wait duration to 10 seconds
-    yield driver                    # 
-    driver.quit()
+    yield driver                    # Return the instanced ChromDriver object at the end of Setup
+    driver.quit()                   # During Cleanup, quit/destroy the instanced ChromeDriver object
 
 
 """
@@ -27,24 +33,30 @@ Test Procedure:
 
 # Define Test Function to Search DuckDuckGo
 def test_basic_duckduckgo_search(browser):
-    # Define Test Variables                 # Arrange / GIVEN
-    URL = 'https://www.duckduckgo.com'
-    PHRASE = 'panda'
+    """ Arrange / GIVEN Section """
+    # Define Test Variables necessary for testing                
+    URL = 'https://www.duckduckgo.com'                                          
+    PHRASE = 'panda'                                                            
+    # Navigate to DuckDuckGo's home page utilizing the instantiated ChromeDriver
+    browser.get(URL)                                                           
 
-    # Navigate to the URL utilizing the instantiated Browser
-    browser.get(URL)                        # Act / WHEN
-
-    # Find the Search Text Field using the Elements Id value
-    search_input = browser.find_element_by_id('search_form_input_homepage')     # Assert / THEN
+    """ Act / WHEN Section """
+    # Find the Search Input Text Field using the Elements Id value                    
+    search_input = browser.find_element_by_id('search_form_input_homepage') # The 'id' attribute = 'search_form_input_homepage' in the DOM
     # Send the Browser the Phrase then press Enter/Return
-    search_input.send_keys(PHRASE + Keys.RETURN)
+    search_input.send_keys(PHRASE + Keys.RETURN)                      
 
-    link_divs = browser.find_elements_by_css_selector('#links > div')
-    assert len(link_divs) > 0
+    """ Assert / THEN Section """
+    # Verify that results(links) appear on the results page after the search
+    link_divs = browser.find_elements_by_css_selector('#links > div')     
+    assert len(link_divs) > 0                                                   
 
-    xpath = f"//div[@id='links']//*[contains(text(), '{PHRASE}')]"
-    results = browser.find_elements_by_xpath(xpath)
-    assert len(results) > 0
+    # Verify that at least one search result contains the search phrase
+    xpath = f"//div[@id='links']//*[contains(text(), '{PHRASE}')]"        
+    phrase_results = browser.find_elements_by_xpath(xpath)          
+    assert len(phrase_results) > 0                                                     
 
+    # Verify that the search phrase is the same
     search_input = browser.find_element_by_id('search_form_input')
-    assert search_input.get_attribute('value') == PHRASE
+    assert search_input.get_attribute('value') == PHRASE 
+
