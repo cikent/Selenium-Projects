f7598909d2d5d87627dbc84ad56450d40b106151
cikent <christopherikent@gmail.com>
2020-03-26 22:37:51 -0700

    Finished converting basic test to Page Object Model/Pattern; fully functional and commented
    Signed-off-by: cikent <christopherikent@gmail.com>

--- Selenium-PythonProjects/python-webui-testing/pages/search.py
+++ Selenium-PythonProjects/python-webui-testing/pages/search.py
@@ -1,24 +1,24 @@
-# Import Selenium Locator modules for the Class object to function
+# Import Selenium locator modules for the Class Object to function
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-# Create a Class Object for the DuckDuckGo home page
-class DuckDuckGoSearchPage:
-    # Create a String variable to hold the Duck Duck Go home page value
+# Create a Class Object for the DuckDuckGo Home page
+class DuckDuckGoSearchPage(object):
+    # Create a String variable to hold the Duck Duck Go Home page value
     URL = 'https://www.duckduckgo.com'
 
-    # Create a Tuple variable to hold the Search Input text field Element Id value 
+    # Create a Tuple variable to hold the Search Input Text Field using the Element Id value named 'search_form_input_homepage'
     SEARCH_INPUT = (By.ID, 'search_form_input_homepage')
 
-    # When the Class is instantiated, initialize the Browser from the pytest fixture  
+    # When the Class is instantiated, initialize the browser from the pytest fixture  
     def __init__(self, browser):
         self.browser = browser
 
-    # Load the DuckDuckGo home page
+    # Load the DuckDuckGo Home page
     def load(self):
         self.browser.get(self.URL)
 
-    # Find the DuckDuckGo Search Input text field & pass in a phrase
+    # Find the DuckDuckGo Search Input Text Field & pass in a phrase
     def search(self, phrase):
         search_input = self.browser.find_element(*self.SEARCH_INPUT)
         search_input.send_keys(phrase + Keys.RETURN)
--- Selenium-PythonProjects/python-webui-testing/tests/test_web.py
+++ Selenium-PythonProjects/python-webui-testing/tests/test_web.py
@@ -1,57 +1,42 @@
-"""
-This module contains web test cases for the tutorial.
-Tests use Selenium WebDriver with Chrome and ChromeDriver.
-The fixtures set up and clean up the ChromeDriver instance.
-"""
-
 # Import the Pytest Library
 import pytest
 
 # Import the Page Objects created within the Project
+from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
-# from pages.result import 
 
 # From the Selenium Webdriver Library, import the Chrome module
 from selenium.webdriver import Chrome
-# From the Selenium Webdriver Common Keys Library, import the Keys module
-from selenium.webdriver.common.keys import Keys
 
 # Create a Pytest Fixture to handle Setup and Cleanup of Tests
-@pytest.fixture                     # Pytest Decorator
-def browser():                      # Define the function
-    driver = Chrome()               # Initialize ChromeDriver by creating a Chrome browser object
-    driver.implicitly_wait(10)      # Set the Driver's Implicit Wait duration to 10 seconds
-    yield driver                    # Return the instanced ChromDriver object at the end of Setup
-    driver.quit()                   # During Cleanup, quit/destroy the instanced ChromeDriver object
+@pytest.fixture                             # Pytest Decorator
+def browser():                              # Define the function
+    driver = Chrome()                       # Initialize ChromeDriver by creating a Chrome browser object
+    driver.implicitly_wait(10)              # Set the Driver's Implicit Wait duration to 10 seconds
+    yield driver                            # Return the instanced ChromDriver object at the end of Setup
+    driver.quit()                           # During Cleanup, quit/destroy the instanced ChromeDriver object
 
 # Define Test Function to Search DuckDuckGo
 def test_basic_duckduckgo_search(browser):
     """ Arrange / GIVEN Section """
-    # Define Test Specifc Search Phrase                
+    # Define a test specifc search phrase (i.e. Set up test case data)               
     PHRASE = 'panda'                                                            
 
+    """ Act / WHEN Section """
+    # Create an instanced Class object from the DuckDuckGoSearchPage Class
     search_page = DuckDuckGoSearchPage(browser)
-    search_page.load
+    # Call the DuckDuckGoSearchPage Load method and navigate to the Duck Duck Go home page
+    search_page.load()
+    # Find the Search Text field and pass in the Search
     search_page.search(PHRASE)
 
-
-    """ Act / WHEN Section """
-    # Find the Search Input Text Field using the Elements Id value                    
-    search_input = browser.find_element_by_id('search_form_input_homepage') # The 'id' attribute = 'search_form_input_homepage' in the DOM
-    # Send the Browser the Phrase then press Enter/Return
-    search_input.send_keys(PHRASE + Keys.RETURN)                      
-
     """ Assert / THEN Section """
-    # Verify that results(links) appear on the results page after the search
-    link_divs = browser.find_elements_by_css_selector('#links > div')     
-    assert len(link_divs) > 0                                                   
-
-    # Verify that at least one search result contains the search phrase
-    xpath = f"//div[@id='links']//*[contains(text(), '{PHRASE}')]"        
-    phrase_results = browser.find_elements_by_xpath(xpath)          
-    assert len(phrase_results) > 0                                                     
-
-    # Verify that the search phrase is the same
-    search_input = browser.find_element_by_id('search_form_input')
-    assert search_input.get_attribute('value') == PHRASE 
+    # Create an instanced Class object from the DuckDuckGoResultsPage Class
+    results_page = DuckDuckGoResultPage(browser)
+    # Verify the 'div' called Links returns more than zero links
+    assert results_page.link_div_count() > 0
+    # Verify the 'div' called Links returns more than zero links that match the search phrase value
+    assert results_page.phrase_result_count(PHRASE) > 0
+    # Verify that the Results page Search Text Field value matches the search phrase
+    assert results_page.search_input_value() == PHRASE
 
